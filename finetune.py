bae8ee071f0afe61f514871ffcbdd06283c82ea5
JJW2602 <jjwook2602@gmail.com>
2025-08-25 19:56:17 +0900

    store dir : NAS

--- finetune.py
+++ finetune.py
@@ -42,13 +42,13 @@ class Workspace:
         utils.set_seed_everywhere(cfg.seed)
         self.device = torch.device(cfg.device)
         self.domain, _ = self.cfg.task.split('_', 1)
-        
+
         if cfg.use_wandb:
             exp_name = '_'.join([
                 cfg.experiment, cfg.task, cfg.agent.name, cfg.obs_type, str(cfg.snapshot_ts),
                 str(cfg.seed), 
             ])
-            wandb.init(project="urlb_finetuning_test", group=cfg.agent.name, name=exp_name)
+            wandb.init(project="urlb_finetuning_walker_icm_diayn", group=cfg.agent.name, name=exp_name)
 
         # create logger
         self.logger = Logger(self.work_dir,
